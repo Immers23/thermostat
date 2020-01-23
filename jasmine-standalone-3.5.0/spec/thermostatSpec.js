f9c5fce19bf718e661bf3cd293ed653e0db81488
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-23 15:54:24 +0000

    thermostat up and down limits created

--- jasmine-standalone-3.5.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/thermostatSpec.js
@@ -1,29 +1,42 @@
 describe('Thermostat', function() {
 
+beforeEach(function() {
+  thermostat = new Thermostat
+});
+
   var thermostat;
 
     describe('when initializing thermostat has a default value', function() {
       it('thermostat has a start temp of 20 degrees', function() {
-
-        thermostat = new Thermostat();
-        expect(thermostat.temp()).toEqual(20);
+        expect(thermostat.temp()).toEqual(thermostat.DEFAULT_TEMP);
       });
+
+      it('has a max temp of 25 degress', function(){
+        for (var i = 0; i < 5; i++) {
+          thermostat.up();
+        };
+        expect(function(){thermostat.up()}).toThrow("max temp reached");
+      });
+
+
+      it('has a mimimum temp of 10 degrees', function(){
+        for (var i = 0; i < 10; i++) {
+          thermostat.down();
+        };
+      expect(function(){thermostat.down()}).toThrow("minimum temp reached");
     });
 
     describe('you can increase and decrease the temp', function() {
       it('can increase the temp with an up function', function() {
-
-        thermostat = new Thermostat();
         thermostat.up();
-        expect(thermostat.temp()).toEqual(21);
+        expect(thermostat.temperature).toEqual(21);
       });
 
 
       it('can decrese the temp with an down function', function() {
-
-        thermostat = new Thermostat();
         thermostat.down();
-        expect(thermostat.temp()).toEqual(19);
+        expect(thermostat.temperature).toEqual(19);
       });
     });
   });
+});
--- jasmine-standalone-3.5.0/src/thermostat.js
+++ jasmine-standalone-3.5.0/src/thermostat.js
@@ -1,13 +1,29 @@
 function Thermostat(){
-  this.defaultTemp = 20;
+  this.DEFAULT_TEMP = 20;
+  this.MINIMUM_TEMP = 10;
+  this.MAXIMUM_TEMP = 25;
+  this.temperature = this.DEFAULT_TEMP;
+
 
 }
 Thermostat.prototype.temp = function(){
-  return this.defaultTemp;
+  return this.temperature;
 };
-Thermostat.prototype.up = function(){
-  return this.defaultTemp+=1;
-};
+// Thermostat.prototype.up = function(){
+//   this.temperature+=1;
+// };
 Thermostat.prototype.down = function(){
-  return this.defaultTemp-=1;
+  if (this.temperature <= this.MINIMUM_TEMP){
+    throw "minimum temp reached"
+  } else {
+    this.temperature-=1
+    }
+  }
+
+Thermostat.prototype.up = function(){
+  if (this.temperature >= this.MAXIMUM_TEMP){
+    throw "max temp reached"
+  } else {
+    this.temperature+=1
+  };
 };
